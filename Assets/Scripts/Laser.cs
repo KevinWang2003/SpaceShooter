f934c4d91c9e13fb98d8b23c3f34efc9e3599b14
Kevin Wang <wang0071@hz.nl>
2025-05-03 00:05:45 +0200

    Updated Main Menu & Added Co-Op and added lasers to enemies.

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -6,20 +6,66 @@ public class Laser : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 8f;
+    private bool _isEnemyLaser = false;
 
     // Update is called once per frame
     void Update()
     {
+        if (!_isEnemyLaser)
+        {
+            MoveUp();
+        }
+        else
+        {
+            MoveDown();
+        }
+    }
+
+    void MoveUp()
+    {
         transform.Translate(_speed * Time.deltaTime * Vector3.up);
 
-        if (transform.position.y > 8)
+        if (transform.position.y > 8f)
         {
             if (transform.parent != null)
             {
                 Destroy(transform.parent.gameObject);
-            }    
+            }
 
             Destroy(this.gameObject);
         }
     }
+
+    void MoveDown()
+    {
+        transform.Translate(_speed * Time.deltaTime * Vector3.down);
+
+        if (transform.position.y < -8f)
+        {
+            if (transform.parent != null)
+            {
+                Destroy(transform.parent.gameObject);
+            }
+
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void AssignEnemyLaser()
+    {
+        _isEnemyLaser = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player" && _isEnemyLaser)
+        {
+            Player player = other.GetComponent<Player>();
+            if (player != null)
+            {
+                player.Damage();
+            }
+        }
+    }
+
 }
